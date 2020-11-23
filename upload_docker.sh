4958c7b5adf050ae82bc56b688078abcb8057c4a
Michael Thiem <mthiem0@gmail.com>
2020-11-23 11:06:10 +0000

    Resolve Project Task 4

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,15 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=mithie/ml-api
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 
+docker login -u mithie
+docker tag ml-api $dockerpath:latest
+
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:latest
